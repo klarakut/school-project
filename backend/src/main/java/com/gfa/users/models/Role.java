f50c85f4c47f43534d25a4c6b73e695ebabb631a
Klara Kutlakova <100863309+klarakut@users.noreply.github.com>
2022-06-24 09:13:54 +0200

    Efdt2 12 role model and entity (#4)
    * EFDT2-12 created entity Role in models package
    
    * in model Role: changed @NotNull annotation for id, added 'name = team_id' for inverseJoinColumns in team_role_db database
    
    * updated model Role
    
    * updated model Role and created unit test for model Role
    
    * resolved pull request comment
    
    * updated model Role and unit test for model Role
    
    * updated the Role entity
    
    * changed the method name from can to is
    
    * updated method can(Permission permission)
    
    * removed wrong imported class Permission

---
+++ backend/src/main/java/com/gfa/users/models/Role.java
@@ -0,0 +1,83 @@
+package com.gfa.users.models;
+
+import org.jetbrains.annotations.NotNull;
+import com.gfa.users.models.Permission;
+import com.gfa.users.models.User;
+import com.gfa.users.models.Team;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true,name = "id")
+    @NotNull
+    private Long id;
+    @NotNull
+    @Column(unique = true,name = "role")
+    private String role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "role_permission_db",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    Set<Permission> permissions;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role_db",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    Set<User> users;
+
+    @ManyToMany
+    @JoinTable(
+            name = "team_role_db",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id"))
+    Set<Team> teams;
+    
+    public Role() {
+    }
+    public Role(@NotNull String role) {
+        this.role = role;
+    }
+
+    @NotNull
+    public Long getId() {
+        return id;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void addPermission(Permission p){
+        permissions.add(p);
+    }
+
+    public void removePermission(Permission p){
+        permissions.remove(p);
+    }
+
+    public Boolean is(Role r){
+        return (role.equals(r.role));
+    }
+
+    public Boolean can(Permission permission){
+        return can(permission.getAbility);
+    }
+
+    public Boolean can(String ability){
+        for(Permission permission : permissions){
+            if(ability.equals(permission.getAbility)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
