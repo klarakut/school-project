508ff24fdfd0d0bd091d5938ebe9e76121be08bc
Martin Gažík <75699289+GaaZko@users.noreply.github.com>
2022-06-30 12:25:22 +0200

    EFDT2-19 tried to create tests

--- backend/src/test/java/com/gfa/users/controllers/UserRestControllerTest.java
+++ backend/src/test/java/com/gfa/users/controllers/UserRestControllerTest.java
@@ -4,43 +4,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gfa.common.dtos.EmailRequestDto;
 import com.gfa.users.models.User;
 import com.gfa.users.repositories.UserRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(UserRestController.class)
+@AutoConfigureMockMvc
 class UserRestControllerTest {
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+  }
 
 
-
-  @Autowired MockMvc mockMvc;
-  @Autowired ObjectMapper objectMapper;
-  @MockBean  UserRepository userRepository;
-  @MockBean
-  EmailRequestDto emailDto;
-
-  User Martin = new User("Martin","email","password",new Date(System.currentTimeMillis()));
-  User Michal = new User("Michal", "michal@gmail.com", "JavaLOver",new Date(System.currentTimeMillis()));
-  User Klara = new User("Klara", "klarakut@seznam.com","UnitLover",new Date(System.currentTimeMillis()));
+  User Martin = new User("Martin", "email", "password", new Date(System.currentTimeMillis()));
+  User Michal =
+      new User("Michal", "michal@gmail.com", "JavaLOver", new Date(System.currentTimeMillis()));
+  User Klara =
+      new User("Klara", "klarakut@seznam.com", "UnitLover", new Date(System.currentTimeMillis()));
 
   EmailRequestDto email = new EmailRequestDto("Gregorovic@gmail.com");
 
-  @Test
-  void shouldCreateMockMvc() {
-    assertNotNull(mockMvc);
-  }
+
 
   @Test
   void store() {}
 
   @Test
-  void resetPassword() throws Exception {
+  public void resetPassword() throws Exception {
     
+
   }
 
   @Test
