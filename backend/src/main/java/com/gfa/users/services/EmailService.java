9ef88a3f89f95a8942adfcae6765f1424b5f46ce
Martin Gažík <75699289+GaaZko@users.noreply.github.com>
2022-07-04 17:27:55 +0200

    EFDT2-22 created class and method

--- backend/src/main/java/com/gfa/users/services/EmailService.java
+++ backend/src/main/java/com/gfa/users/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.gfa.users.services;
 
-
 import com.gfa.users.EmailConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -9,32 +8,30 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class EmailService {
 
-    @Autowired
-   private final  EmailConfiguration emailConfiguration;
+  @Autowired private final EmailConfiguration emailConfiguration;
 
-    public EmailService(EmailConfiguration emailConfiguration) {
-        this.emailConfiguration = emailConfiguration;
-    }
+  public EmailService(EmailConfiguration emailConfiguration) {
+    this.emailConfiguration = emailConfiguration;
+  }
 
 
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
-        mailSenderImpl.setHost(emailConfiguration.getHost());
-        mailSenderImpl.setPort(emailConfiguration.getPort());
-        mailSenderImpl.setUsername(emailConfiguration.getUsername());
-        mailSenderImpl.setPassword(emailConfiguration.getPassword());
-        return mailSenderImpl;
-    }
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+    mailSenderImpl.setHost(emailConfiguration.getHost());
+    mailSenderImpl.setPort(emailConfiguration.getPort());
+    mailSenderImpl.setUsername(emailConfiguration.getUsername());
+    mailSenderImpl.setPassword(emailConfiguration.getPassword());
+    return mailSenderImpl;
+  }
 
-    public void sendSimpleMessage(String to, String from, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        message.setFrom(from);
-        getJavaMailSender().send(message);
-    }
+  public void sendSimpleMessage(String to, String from, String subject, String text) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(text);
+    message.setFrom(from);
+    getJavaMailSender().send(message);
+  }
 }
