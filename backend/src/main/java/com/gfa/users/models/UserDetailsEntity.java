1c2f791af336d47bc6ececfdc0aecccef3c47434
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-07 15:01:24 +0200

    created class UserDetailsEntity

---
+++ backend/src/main/java/com/gfa/users/models/UserDetailsEntity.java
@@ -0,0 +1,43 @@
+package com.gfa.users.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsEntity extends User  implements UserDetails {
+
+    /*public UserDetailsEntity(final User user){
+        super(user);
+    }*/
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
