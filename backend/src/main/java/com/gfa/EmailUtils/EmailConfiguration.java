f548c691cb12363ff02cf3125154db5c1f17116b
Martin Gažík <75699289+GaaZko@users.noreply.github.com>
2022-07-07 09:49:29 +0200

    EFDT2-22 created method for HTML email

--- backend/src/main/java/com/gfa/EmailUtils/EmailConfiguration.java
+++ backend/src/main/java/com/gfa/EmailUtils/EmailConfiguration.java
@@ -1,4 +1,4 @@
-package com.gfa.users;
+package com.gfa.EmailUtils;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- backend/src/main/java/com/gfa/EmailUtils/EmailService.java
+++ backend/src/main/java/com/gfa/EmailUtils/EmailService.java
@@ -1,16 +1,24 @@
-package com.gfa.users.services;
+package com.gfa.EmailUtils;
 
-import com.gfa.users.EmailConfiguration;
+import com.gfa.EmailUtils.EmailConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 
   @Autowired private final EmailConfiguration emailConfiguration;
+  private JavaMailSender javaMailSender;
+
+
 
   public EmailService(EmailConfiguration emailConfiguration) {
     this.emailConfiguration = emailConfiguration;
@@ -26,12 +34,23 @@ public class EmailService {
     return mailSenderImpl;
   }
 
-  public void sendSimpleMessage(String to, String from, String subject, String text) {
+  public void sendPlainEmail(String to, String from, String subject, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
     message.setTo(to);
     message.setSubject(subject);
     message.setText(text);
     message.setFrom(from);
     getJavaMailSender().send(message);
+  }
+
+  public void sendHtmlEmail(String to, String from, String subject, String text) throws MessagingException {
+    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,"utf-8");
+    mimeMessage.setContent(text, "text/html");
+    mimeMessageHelper.setFrom(from);
+    mimeMessageHelper.setTo(to);
+    mimeMessageHelper.setSubject(subject);
+    javaMailSender.send(mimeMessage);
+
   }
 }
