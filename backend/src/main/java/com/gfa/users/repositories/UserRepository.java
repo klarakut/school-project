3a4618e736559c6a1273bd16aa8952895d7e8627
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-07 16:28:55 +0200

    added method findByUsername() to the UserRepository

--- backend/src/main/java/com/gfa/users/repositories/UserRepository.java
+++ backend/src/main/java/com/gfa/users/repositories/UserRepository.java
@@ -12,8 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmail(String email);
-    Optional<Object> findByUsernameOrEmail(String username, String email);
-    Optional<Object> findByUsername(String username);
+    Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
