a0e232fabda7319febb8c4b7e29eefc48084fad9
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-07 16:24:47 +0200

    created class 'JwtConfiguration' in service

---
+++ backend/src/main/java/com/gfa/users/security/JwtConfiguration.java
@@ -0,0 +1,44 @@
+package com.gfa.users.security;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class JwtConfiguration {
+
+    @Value("${security.jwt.uri:}")
+    private String Uri;
+
+    @Value("${security.jwt.header:}")
+    private String header;
+
+    @Value("${security.jwt.prefix:}")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:1234}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:}")
+    private String secret;
+
+    public JwtConfiguration() {
+    }
+
+    public String getUri() {
+        return Uri;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public int getExpiration() {
+        return expiration;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+}
