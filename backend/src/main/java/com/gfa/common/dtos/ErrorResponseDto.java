15bb8edf0759d620e1687cba3d563ae5d16b6b37
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-07 17:04:03 +0200

    updated dtos

--- backend/src/main/java/com/gfa/common/dtos/ErrorResponseDto.java
+++ backend/src/main/java/com/gfa/common/dtos/ErrorResponseDto.java
@@ -1,9 +1,10 @@
 package com.gfa.common.dtos;
 
-public class ErrorResponseDto {
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class ErrorResponseDto extends ResponseDto{
   public final String message;
 
-  @JsonCreator
   public ErrorResponseDto(String message) {
     this.message = message;
   }
