96979074cfdb38781c3e6a4e73046dd72842d6fb
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-10 16:55:00 +0200

    EFDT2-25 created dto class 'RoleCreateRequestDto'

---
+++ backend/src/main/java/com/gfa/common/dtos/RoleCreateRequestDto.java
@@ -0,0 +1,10 @@
+package com.gfa.common.dtos;
+
+public class RoleCreateRequestDto {
+
+    public final String role;
+
+    public RoleCreateRequestDto(String role) {
+        this.role = role;
+    }
+}
