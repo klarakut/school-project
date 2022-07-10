a7be1da38c3d6db8a2d4c33928201be604c25eca
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-10 16:53:03 +0200

    EFDT2-25 created dto class 'RoleRequestDto'

---
+++ backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
@@ -0,0 +1,18 @@
+package com.gfa.common.dtos;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class RoleRequestDto {
+
+    public final Long id;
+    public final String role;
+
+    //@JsonProperty(name="")
+
+    @JsonCreator
+    protected RoleRequestDto(Long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+}
