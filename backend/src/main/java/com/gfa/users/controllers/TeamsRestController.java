bb19a31476208953ba8126c8fefdd2d755788fd7
Martin Gažík <75699289+GaaZko@users.noreply.github.com>
2022-07-11 10:40:37 +0200

    EFDT2-27 create a restController

---
+++ backend/src/main/java/com/gfa/users/controllers/TeamsRestController.java
@@ -0,0 +1,15 @@
+package com.gfa.users.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamsRestController {
+
+
+    @GetMapping("/teams")
+    public 
+
+
+}
