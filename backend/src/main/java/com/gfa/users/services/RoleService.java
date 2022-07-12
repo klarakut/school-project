07ebe81bdd2c692fcaeaa68ee14da24a6cb369d7
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-12 13:31:58 +0200

    EFDT2-25: added methods 'storePermission' and 'destroyPermission' in the class 'RoleService'

--- backend/src/main/java/com/gfa/users/services/RoleService.java
+++ backend/src/main/java/com/gfa/users/services/RoleService.java
@@ -1,9 +1,6 @@
 package com.gfa.users.services;
 
-import com.gfa.common.dtos.RoleCreateRequestDto;
-import com.gfa.common.dtos.RolePatchRequestDto;
-import com.gfa.common.dtos.RoleRequestDto;
-import com.gfa.common.dtos.RoleResponseDto;
+import com.gfa.common.dtos.*;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -14,4 +11,8 @@ public interface RoleService {
     RoleResponseDto show(Long id);
     List<RoleResponseDto> index();
     void destroy(Long id);
+
+    void storePermission(Long id, PermissionRequestDto permission);
+
+    void destroyPermission(Long id, Long permissionId);
 }
