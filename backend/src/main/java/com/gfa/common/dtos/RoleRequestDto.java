9a65b35d33e5ee2a5055639cf6858389725c27b9
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-12 13:38:09 +0200

    EFDT2-25:updated the class 'RoleRequestDto' from abstract to ordinary class

--- backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
+++ backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
@@ -3,7 +3,7 @@ package com.gfa.common.dtos;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public abstract class RoleRequestDto {
+public class RoleRequestDto {
 
     public final Long id;
     public final String role;
