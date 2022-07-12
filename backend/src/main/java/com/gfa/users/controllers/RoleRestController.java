797144cc1a6b8d8738f6d3304e0ef1bcb0a00c0d
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-12 13:39:14 +0200

    EFDT2-25: added endpoints 'storePermission' and 'destroyPermission' in the class 'RoleRestController'

--- backend/src/main/java/com/gfa/users/controllers/RoleRestController.java
+++ backend/src/main/java/com/gfa/users/controllers/RoleRestController.java
@@ -28,12 +28,12 @@ public class RoleRestController {
 
     // #POST /roles/
     @PostMapping("/roles")
-    public ResponseEntity<? extends RoleResponseDto> store(@RequestBody RoleCreateRequestDto dto){
+    public ResponseEntity<? extends ResponseDto> store(@RequestBody RoleCreateRequestDto dto){
         try{
             RoleResponseDto role = roleService.store(dto);
             return new ResponseEntity<>(role,HttpStatus.CREATED);
         }
-        catch (RoleMissingException e){
+        catch (EmptyBodyException e){
             return new ResponseEntity<>(new ErrorResponseDto("Role is required"), HttpStatus.BAD_REQUEST);
         }
         catch (RoleExistException e){
@@ -53,7 +53,7 @@ public class RoleRestController {
 
     // #GET /roles/{id}
     @GetMapping("/roles/{id}")
-    public ResponseEntity<? extends RoleResponseDto> show(@PathVariable("id") Long id){
+    public ResponseEntity<? extends ResponseDto> show(@PathVariable("id") Long id){
         try {
             RoleResponseDto responseDto = roleService.show(id);
             return new ResponseEntity<>(responseDto,HttpStatus.resolve(200));
@@ -68,7 +68,7 @@ public class RoleRestController {
 
     // #PUT
     @PutMapping("/roles/{id}")
-    public ResponseEntity<? extends RoleResponseDto> update(@PathVariable("id") Long id, @RequestBody RolePatchRequestDto dto){
+    public ResponseEntity<? extends ResponseDto> update(@PathVariable("id") Long id, @RequestBody RolePatchRequestDto dto){
         try {
             RoleResponseDto responseDto = roleService.update(id,dto);
             return new ResponseEntity<>(responseDto,HttpStatus.resolve(200));
@@ -79,8 +79,8 @@ public class RoleRestController {
         catch (IdNotFoundException e){
             return new ResponseEntity<>(new ErrorResponseDto("Role not found"),HttpStatus.NOT_FOUND);
         }
-        catch (InvalidBodyException e){
-            return new ResponseEntity<>(new ErrorResponseDto("Invalid data",HttpStatus.BAD_REQUEST))
+        catch (InvalidInputException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Invalid data"),HttpStatus.BAD_REQUEST);
         }
         catch (UnknownErrorException e){
             return new ResponseEntity<>(new ErrorResponseDto("Unknown error"), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -96,7 +96,7 @@ public class RoleRestController {
 
     // #DELETE
     @DeleteMapping("/roles/{id}")
-    public ResponseEntity<? extends RoleResponseDto> destroy(@PathVariable("id") Long id){
+    public ResponseEntity<? extends ResponseDto> destroy(@PathVariable("id") Long id){
         try {
             roleService.destroy(id);
             return new ResponseEntity<>(HttpStatus.resolve(201));
@@ -107,5 +107,66 @@ public class RoleRestController {
         catch (IdNotFoundException e){
             return new ResponseEntity<>(new ErrorResponseDto("Role not found"),HttpStatus.NOT_FOUND);
         }
+        // TODO
+        /*catch (HttpClientErrorException.Unauthorized e){
+            return new ResponseEntity<>(new ErrorResponseDto("Unauthorized user"), HttpStatus.UNAUTHORIZED);
+        }
+        catch (HttpClientErrorException.Forbidden e){
+            return new ResponseEntity<>(new ErrorResponseDto("Insufficient rights to create roles"), HttpStatus.FORBIDDEN);
+        }*/
+    }
+
+    // #POST
+    @PostMapping("roles/{id}/permissions")
+    public ResponseEntity<? extends ResponseDto> storePermission(@PathVariable ("id") Long id, @RequestBody PermissionRequestDto permission){
+        try {
+            roleService.storePermission(id,permission);
+            return new ResponseEntity<>(HttpStatus.resolve(200));
+        }
+        catch (InvalidInputException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Invalid data"), HttpStatus.BAD_REQUEST);
+        }
+        catch (IdNotFoundException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Role not found"), HttpStatus.NOT_FOUND);
+        }
+        catch (UnknownErrorException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Unknown error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        // TODO
+        /*catch (HttpClientErrorException.Unauthorized e){
+            return new ResponseEntity<>(new ErrorResponseDto("Unauthorized user"), HttpStatus.UNAUTHORIZED);
+        }
+        catch (HttpClientErrorException.Forbidden e){
+            return new ResponseEntity<>(new ErrorResponseDto("Insufficient rights to create roles"), HttpStatus.FORBIDDEN);
+        }*/
+    }
+
+    //#DELETE
+    @DeleteMapping("roles/{id}/permissions/{permission_id}")
+    public ResponseEntity<? extends ResponseDto> destroyPermission(@PathVariable ("id") Long id, @PathVariable ("permissionId") Long permissionId){
+        try {
+            roleService.destroyPermission(id,permissionId);
+            return new ResponseEntity<>(HttpStatus.resolve(204));
+        }
+        catch (IdNotFoundException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Role not found"),HttpStatus.NOT_FOUND);
+        }
+        catch (PermissionIdNotFoundException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Permission not found"),HttpStatus.NOT_FOUND);
+        }
+        catch (InvalidInputException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Invalid data"), HttpStatus.BAD_REQUEST);
+        }
+        catch (UnknownErrorException e){
+            return new ResponseEntity<>(new ErrorResponseDto("Unknown error"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // TODO
+        /*catch (HttpClientErrorException.Unauthorized e){
+            return new ResponseEntity<>(new ErrorResponseDto("Unauthorized user"), HttpStatus.UNAUTHORIZED);
+        }
+        catch (HttpClientErrorException.Forbidden e){
+            return new ResponseEntity<>(new ErrorResponseDto("Insufficient rights to create roles"), HttpStatus.FORBIDDEN);
+        }*/
     }
 }
