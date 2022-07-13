c00542c7b075107b64d7bae014f04808bf2098d9
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-13 22:04:53 +0200

    EFDT2-21:created unit tests in UserRestControllerTestEndpointRegister

--- backend/src/test/java/com/gfa/users/controllers/UserRestControllerTestEndpointRegister.java
+++ backend/src/test/java/com/gfa/users/controllers/UserRestControllerTestEndpointRegister.java
@@ -7,7 +7,6 @@ import com.gfa.users.exceptions.*;
 import com.gfa.users.models.User;
 import com.gfa.users.repositories.UserRepository;
 import com.gfa.users.services.UserService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,26 +14,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,7 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @ContextConfiguration
 @AutoConfigureMockMvc
-class UserRestControllerTest {
+class UserRestControllerTestEndpointRegister {
 
     @Autowired
     private MockMvc mvc;
