89081f5e4ebe8a09bef9e14eb5741647164f5842
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-13 22:02:02 +0200

    EFDT2-21:deleted unnecessary code from 'UserDetailsEntity'

--- backend/src/main/java/com/gfa/users/models/UserDetailsEntity.java
+++ backend/src/main/java/com/gfa/users/models/UserDetailsEntity.java
@@ -10,13 +10,6 @@ import java.util.stream.Collectors;
 
 public class UserDetailsEntity extends User implements UserDetails {
 
-    // security User
-   // private User user;
-
-    /*public UserDetailsEntity(User user) {
-        super();
-        this.user = user;
-    }*/
     public UserDetailsEntity(final User user){
         super(user);
     }
@@ -24,14 +17,7 @@ public class UserDetailsEntity extends User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
-        /*return user.getAuthorities()
-                .stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole))
-                .collect(Collectors.toSet());
-*/
-       // return Collections.singleton(new SimpleGrantedAuthority(user.getRoles()));
-
-    /*return getRoles()
+        /*return  getRoles()
                 .stream()
                 .map(role -> new SimpleGrantedAuthority(role.getRole()))
                 .collect(Collectors.toSet());*/
@@ -56,42 +42,4 @@ public class UserDetailsEntity extends User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
-    //extends User
-
-
-
-
-    /*public UserDetailsEntity(final User user){
-        super(user);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-        /*return getRoles()
-                .stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
-                .collect(Collectors.toSet());*/
-  /*  }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-    */
 }
