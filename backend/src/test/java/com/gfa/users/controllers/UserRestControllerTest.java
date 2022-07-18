506a0594b8c2d96299fb85bfa6923adef202ee0e
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-18 23:33:29 +0200

    EFDT2-21: updated UserRestController

--- backend/src/test/java/com/gfa/users/controllers/UserRestControllerTest.java
+++ backend/src/test/java/com/gfa/users/controllers/UserRestControllerTest.java
@@ -1,11 +1,13 @@
 package com.gfa.users.controllers;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.gfa.users.dtos.UserResponseDto;
 import com.gfa.users.models.User;
+import com.gfa.users.repositories.UserRepository;
 import com.gfa.users.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -14,31 +16,33 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(
-    value = UserRestController.class,
-    excludeAutoConfiguration = SecurityAutoConfiguration.class)
+        value = UserRestController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class UserRestControllerTest {
 
   @Autowired private MockMvc mvc;
 
   @MockBean UserService userService;
+  @MockBean UserDetailsService userDetailsService;
 
   @Test
   void can_register() throws Exception {
 
     Mockito.when(userService.store(Mockito.any()))
-        .thenReturn(new UserResponseDto(new User("username", "email@domain.com", "password")));
+            .thenReturn(new UserResponseDto(new User("username", "email@domain.com", "password"),"x"));
 
     mvc.perform(
-            MockMvcRequestBuilders.post("/register")
-                .content(
-                    "{\"username\": \"username\", \"email\": \"email@domain.com\", \"password\": \"password\"}")
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.username", is("username")))
-        .andExpect(jsonPath("$.email", is("email@domain.com")));
+                    MockMvcRequestBuilders.post("/register")
+                            .content(
+                                    "{\"username\": \"username\", \"email\": \"email@domain.com\", \"password\": \"password\"}")
+                            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.username", is("username")))
+            .andExpect(jsonPath("$.email", is("email@domain.com")));
   }
 }
