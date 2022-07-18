b5386a67ea13face0e899b0712319940e26900c4
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-18 10:04:19 +0200

    EFDT2-25: added constructor in RoleResponseDto

--- backend/src/main/java/com/gfa/common/dtos/RoleResponseDto.java
+++ backend/src/main/java/com/gfa/common/dtos/RoleResponseDto.java
@@ -1,5 +1,7 @@
 package com.gfa.common.dtos;
 
+import com.gfa.users.models.Role;
+
 public class RoleResponseDto extends ResponseDto {
 
     public final Long id;
@@ -8,5 +10,10 @@ public class RoleResponseDto extends ResponseDto {
     public RoleResponseDto(Long id, String role) {
         this.id = id;
         this.role = role;
+    }
+
+    public RoleResponseDto(Role role) {
+        this.id = role.getId();
+        this.role = role.getRole();
     }
 }
