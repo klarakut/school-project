9c56add20002df72cb6a8153bd1c2e74c0fcab51
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-18 23:31:16 +0200

    EFDT2-21:deleted unnecessary import in UsersApplication

--- backend/src/main/java/com/gfa/users/UsersApplication.java
+++ backend/src/main/java/com/gfa/users/UsersApplication.java
@@ -1,6 +1,5 @@
 package com.gfa.users;
 
-import com.gfa.foxdining.FoxDiningApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
