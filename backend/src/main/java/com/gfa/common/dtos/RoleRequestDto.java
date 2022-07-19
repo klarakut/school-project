7583965ae9e95f2b98e8aa52e214ea9ff7169451
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-19 14:38:01 +0200

    EFDT2-25: removed comment and whitespaces in the class RoleRequestDto

--- backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
+++ backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
@@ -1,18 +1,14 @@
 package com.gfa.common.dtos;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RoleRequestDto {
+  public final Long id;
+  public final String role;
 
-    public final Long id;
-    public final String role;
-
-    //@JsonProperty(name="")
-
-    @JsonCreator
-    protected RoleRequestDto(Long id, String role) {
-        this.id = id;
-        this.role = role;
-    }
+  @JsonCreator
+  protected RoleRequestDto(Long id, String role) {
+    this.id = id;
+    this.role = role;
+  }
 }
