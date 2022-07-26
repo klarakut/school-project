a1f92ded0e62d2886f4f924977e6af996b092e64
KlaraKutlakova <klarakutlakova@gmail.com>
2022-07-26 10:42:01 +0200

    EFDT2-25 created new constructor in the class RoleRequestDto

--- backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
+++ backend/src/main/java/com/gfa/common/dtos/RoleRequestDto.java
@@ -5,7 +5,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public class RoleRequestDto extends RoleDto implements RequestDto {
 
   @JsonCreator
-  protected RoleRequestDto(Long id, String role) {
+  public RoleRequestDto(Long id, String role) {
     super(id,role);
+  }
+
+  @JsonCreator
+  public RoleRequestDto(String role) {
+    super(null,role);
   }
 }
